02035a738119b34c10735082cb700f59b3447026
Ross McFarland <rwmcfa1@gmail.com>
2013-04-21 10:25:41 -0700

    caching

--- www/urls.py
+++ www/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.decorators.cache import cache_page
 from www.api.views import AgencyDetail, RegionDetail, RegionList, \
     RouteDetail, RouteStopDetail
 
@@ -18,17 +19,21 @@ urlpatterns = patterns('',
 
 urlpatterns += patterns('www.api.views',
                         url(r'^api/$', 'api_root', name='api_root'),
-                        url(r'^api/regions/$', RegionList.as_view(),
+                        url(r'^api/regions/$',
+                            cache_page(RegionList.as_view(), 60 * 60),
                             name='regions-list'),
                         url(r'^api/regions/(?P<pk>[\w-]+)/$',
-                            RegionDetail.as_view(), name='region-detail'),
+                            cache_page(RegionDetail.as_view(), 60 * 60),
+                            name='region-detail'),
                         url(r'^api/regions/(?P<region>[\w-]+)'
                             r'/agencies/(?P<pk>[\w-]+)/$',
-                            AgencyDetail.as_view(), name='agency-detail'),
+                            cache_page(AgencyDetail.as_view(), 60 * 60),
+                            name='agency-detail'),
                         url(r'^api/regions/(?P<region>[\w-]+)'
                             r'/agencies/(?P<agency>[\w-]+)'
                             r'/routes/(?P<pk>[\w-]+)/$',
-                            RouteDetail.as_view(), name='route-detail'),
+                            cache_page(RouteDetail.as_view(), 60 * 5),
+                            name='route-detail'),
                         url(r'^api/regions/(?P<region>[\w-]+)'
                             r'/agencies/(?P<agency>[\w-]+)'
                             r'/routes/(?P<route>[\w-]+)'
