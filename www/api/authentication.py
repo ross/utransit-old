23afa7377600c0b9f80a3e1430868ba69d042ae6
Ross McFarland <rwmcfa1@gmail.com>
2013-04-23 21:15:23 -0700

    option token in debug, cleaned up token logic

--- www/api/authentication.py
+++ www/api/authentication.py
@@ -2,18 +2,20 @@
 #
 #
 
+from django.conf import settings
 from rest_framework import exceptions
 from rest_framework.authentication import TokenAuthentication
 
 class APIKeyAuthentication(TokenAuthentication):
 
     def authenticate(self, request):
+
         token = request.GET.get('token', None)
-
-        if not token:
+        if token:
+            ret = self.authenticate_credentials(token)
+        else:
             ret = super(APIKeyAuthentication, self).authenticate(request)
-            if not ret:
-                raise exceptions.AuthenticationFailed('Token required')
 
-
-        return self.authenticate_credentials(token)
+        if not ret and not settings.DEBUG:
+            raise exceptions.AuthenticationFailed('Token required')
+        return ret
