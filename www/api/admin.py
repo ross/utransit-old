24a51a925625c677816e34820ac0398f59e2b392
Ross McFarland <rwmcfa1@gmail.com>
2013-05-01 15:10:54 -0700

    remove old/dead code

--- www/api/admin.py
+++ www/api/admin.py
@@ -5,25 +5,6 @@
 from django import forms
 from django.contrib import admin
 from rest_framework.authtoken.models import Token
-from www.api.models import Agency, Region
-
-
-class RegionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    model = Region
-    search_fields = ('=id', 'name')
-
-
-admin.site.register(Region, RegionAdmin)
-
-
-class AgencyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
-    model = Agency
-    search_fields = ('=region__id', 'id', 'name')
-
-
-admin.site.register(Agency, AgencyAdmin)
 
 
 class TokenAdmin(admin.ModelAdmin):
